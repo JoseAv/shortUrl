908b6237316836e0e59cdbbc1cfd04e4c3bb09a0
JoseAv <jm.aranav@gmail.com>
2025-12-26 15:40:21 -0600

    Agregado repositorio funcionando con la db

--- server/index.ts
+++ server/index.ts
@@ -2,6 +2,7 @@ import express from 'express'
 import type { Request, Response } from 'express'
 import { ComprobationUrl } from './schema/comprobateionSchema.js'
 import { shortUrl } from './repositories/shorUrl.js'
+import { envData } from './config/const/env.js'
 
 const app = express()
 const Port = 3000
@@ -12,14 +13,12 @@ app.post('/', async (req: Request, res: Response) => {
     try {
         const url = req.body.url
         const parseUrl = await ComprobationUrl({ url })
-        await shortUrl({ url: parseUrl })
-
-
-        res.status(200).json({ message: 'Creado con existo' })
-
+        const responseDB = await shortUrl({ url: parseUrl })
+        const createNewUrl = envData.baseUrl + responseDB
+        res.status(200).json({ message: 'Creado con existo', url: createNewUrl })
     } catch (err) {
         console.log(err)
-        res.status(400).json({ message: 'Error en crear la url' })
+        res.status(400).json({ message: 'FIN DE LA URL' })
     }
 })
 
--- server/repositories/shorUrl.ts
+++ server/repositories/shorUrl.ts
@@ -1,17 +1,26 @@
 import { QueryTypes } from "sequelize";
 import { postgres } from "../config/postgres/db.js";
 import type { typeUrl } from "../interfaces/schema.js";
+type resulDb = {
+    result: string
+}
+
 
 export const shortUrl = async ({ url }: { url: typeUrl }) => {
 
     try {
 
-        return await postgres.query('call p_create_url(:url)', {
-            replacements: { url },
+        const responseDB: resulDb[] = await postgres.query('select fn_create_url(:url) as result', {
+            replacements: { url: url.url },
             type: QueryTypes.SELECT,
         })
 
+        if (!responseDB || !responseDB[0] || !(responseDB[0].result)) {
+            throw new Error('No se genero el codigo')
+        }
+        return responseDB[0].result
     } catch (Err) {
+        console.log(Err)
         throw new Error('Fallo la creacion de la url')
     }
 
