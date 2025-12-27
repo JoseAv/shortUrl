7d46ccb24cc0b0353d673bd140154decd5977fa7
JoseAv <jm.aranav@gmail.com>
2025-12-26 22:44:45 -0600

    Agregado front, se eligio colores, tama;os y estilos

---
+++ frontend/src/App.tsx
@@ -0,0 +1,17 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <body>
+      <h1>Hola mundo</h1>
+      <h2>Hola Mundo</h2>
+      <h3>Hola Mundo</h3>
+      <h4>Hola Mundo</h4>
+      <p>Hola Mundo</p>
+      <span>Hola Mundo</span>
+    </body>
+  )
+}
+
+export default App
